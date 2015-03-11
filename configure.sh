5badeb02611107f3c33c08020bd15c7b4995b3d5
jiyang <jiyang@microsoft.com>
2015-03-11 12:35:45 +0800

    update travis

--- configure.sh
+++ configure.sh
@@ -1,10 +1,12 @@
-sudo apt-get install libboost1.54-all-dev libssl-dev cmake libxml++2.6-dev uuid-dev libunittest++-dev
+# Download casabalnca
 rm -fR casablanca
 mkdir casablanca
 cd casablanca
 wget https://wastoragebld.blob.core.windows.net/libcpprest/libcpprest_2_3.tar.gz
 tar xvzf libcpprest_2_3.tar.gz
 cd ..
+
+# Generate build script
 mkdir Microsoft.WindowsAzure.Storage/build.release
 cd Microsoft.WindowsAzure.Storage/build.release
 cmake .. -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTS=ON -DBUILD_SAMPLES=ON
