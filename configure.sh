401e6dfe56791dd066d04b476cfb9ee8ec076c63
Zachary Yang <slepox@users.noreply.github.com>
2015-04-02 10:37:42 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -8,15 +8,8 @@ ln -s libcpprest.so.2.3 libcpprest.so
 cd ..
 
 # Generate build script
-<<<<<<< HEAD
 rm -fR Microsoft.WindowsAzure.Storage/build.*
 mkdir Microsoft.WindowsAzure.Storage/build.debug
 cd Microsoft.WindowsAzure.Storage/build.debug
 cmake .. -DCMAKE_BUILD_TYPE=Debug -DBUILD_TESTS=ON -DBUILD_SAMPLES=ON
-=======
-rm -fR Microsoft.WindowsAzure.Storage/build.release
-mkdir Microsoft.WindowsAzure.Storage/build.release
-cd Microsoft.WindowsAzure.Storage/build.release
-cmake .. -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTS=ON -DBUILD_SAMPLES=ON
->>>>>>> aafbf5067dadc3062a99b52e7cfc360814486575
 cd ../..
